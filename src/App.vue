98cb1b072e391137d8758937b94709c3f636f15f
Leandro.Carolino <carolino.leandro@gmail.com>
2018-09-10 21:52:31 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <h1>{{ title }}</h1>
+    <Navbar />
+    <AllFriends :friends="friends" />
+    <OnlineFriends :friends="friends" />
+  </div>
+</template>
+
+<script>
+import Navbar from './Navbar'
+import AllFriends from './AllFriends'
+import OnlineFriends from './OnlineFriends'
+
+export default {
+  name: 'app',
+  components: {
+    Navbar,
+    OnlineFriends,
+    AllFriends
+  },
+  data () {
+    return {
+      title: 'this is my app!',
+       friends:[
+             
+                 { name: 'Mario', online: true },
+                 { name: 'Luigi', online: true },
+                 { name: 'puck', online: true },
+                 { name: 'glauber', online: true }
+             ]
+    }
+  }
+}
+</script>
+
+<style>
+h1{
+  color:#444;
+  font-weight: normal;
+  text-align: center;
+}
+</style>
---
+++ src/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+    <nav>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Home</a></li>
+        </ul>
+    </nav>
+</template>
+<script>
+export default {
+    name: 'Navbar',
+    data () {
+        return{
+
+        }
+    }
+    
+}
+</script>
+<style>
+nav{
+    text-align: center;
+}
+    ul{
+        list-style: none;
+        padding: 10px;
+    }
+    ul li {
+        display:inline-block;
+        margin: 0;
+    }
+</style>
+
