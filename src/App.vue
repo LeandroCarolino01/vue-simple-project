3cd3a852c1d5de049315f9e30a84a670699d04e8
Leandro.Carolino <carolino.leandro@gmail.com>
2018-09-13 19:46:43 +0200

    filtered post and axios added

--- src/App.vue
+++ src/App.vue
@@ -2,42 +2,28 @@
   <div id="app">
     <h1>{{ title }}</h1>
     <Navbar />
-    <AllFriends :friends="friends" @delete="deleteFriend"/>
-    <OnlineFriends :friends="friends" />
+    <Blogs />
   </div>
 </template>
 
 <script>
 import Navbar from './Navbar'
-import AllFriends from './AllFriends'
-import OnlineFriends from './OnlineFriends'
+import Blogs from './Blogs'
 
 export default {
   name: 'app',
   components: {
     Navbar,
-    OnlineFriends,
-    AllFriends
+    Blogs
   },
   data () {
     return {
-      title: 'this is my app!',
-       friends:[
-             
-                 { name: 'Mario', online: false },
-                 { name: 'Luigi', online: false },
-                 { name: 'puck', online: true },
-                 { name: 'glauber', online: true }
-             ]
+      title: 'this is my app!'
     }
   },
   methods: {
-    deleteFriend(payload){
-      console.log(payload);
-      this.friends = this.friends.filter(friend => {
-        return friend.name !== payload.name
-      })
-    }
+    
+    
   }
 }
 </script>
